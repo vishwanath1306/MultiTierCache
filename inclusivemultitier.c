645f86fe9e24971fc313f40198a8c02d8bff9fdf
thebrahminator <vishwa.nath@outlook.com>
2020-09-13 23:45:50 -0400

    Added: Inclusive caching for block traces
    * Added ability to simulate 2-tier for block traces
    * Added gitignore

---
+++ inclusivemultitier.c
@@ -0,0 +1,138 @@
+#include<stdio.h>
+#include<libCacheSim.h>
+
+
+typedef struct{
+    int tier_1_read_hit;
+    int tier_2_read_hit;
+
+    int tier_1_write_hit;
+    int tier_2_write_hit;
+    
+    int tier_1_read_miss;
+    int tier_2_read_miss;
+
+    int tier_1_write_miss;
+    int tier_2_write_miss;
+    
+    int tier_1_total_hit;
+    int tier_1_total_miss;
+    
+    int tier_2_total_hit;
+    int tier_2_total_miss;
+
+    int read_count;
+    int write_count;
+    int total_count;
+    
+}traceStats;
+
+
+void compute_hits(char* filename, uint64_t tier_1_size, uint64_t tier_2_size, char* op_fname){
+    
+    reader_init_param_t init_csv = {
+        .delimiter=',',
+        .obj_id_field=1,
+        .op_field=2,
+        .has_header=false
+    };
+
+    reader_t *reader_csv = open_trace(filename, CSV_TRACE, OBJ_ID_NUM, &init_csv);
+    request_t *req = new_request();
+
+    common_cache_params_t tier_1_params = {.cache_size=tier_1_size};
+    common_cache_params_t tier_2_params = {.cache_size=tier_2_size};
+
+    cache_t *tier_1_cache = create_cache("LRU", tier_1_params, NULL);
+    cache_t *tier_2_cache = create_cache("LRU", tier_2_params, NULL);
+
+    traceStats multi_tier_stats = {0,0,0,0,0,0,0,0,0};
+
+    while(read_one_req(reader_csv, req) == 0){
+        cache_ck_res_e tier_1_check = tier_1_cache->check(tier_1_cache, req, true);
+        cache_ck_res_e tier_2_check = tier_2_cache->check(tier_2_cache, req, true); 
+        
+        printf("Request is: %ld, Request type: %d\n", req->obj_id_int, req->op);
+        if((tier_1_check == cache_ck_miss) && (tier_2_check == cache_ck_miss)){
+            // Condition where the object is not found in both layers.             
+            tier_1_cache->get(tier_1_cache, req);
+            tier_2_cache->get(tier_2_cache, req);   
+            if(req->op == OP_READ){
+                multi_tier_stats.tier_1_read_miss++;
+                multi_tier_stats.tier_2_read_miss++;
+                multi_tier_stats.read_count++;
+            }else{
+                multi_tier_stats.tier_1_write_miss++;
+                multi_tier_stats.tier_2_write_miss++;
+                multi_tier_stats.write_count++;
+            }
+            multi_tier_stats.total_count++;
+            multi_tier_stats.tier_1_total_miss++;
+            multi_tier_stats.tier_2_total_miss++;
+        }
+
+        if((tier_1_check == cache_ck_miss) && (tier_2_check != cache_ck_miss)){
+            // Condition where the object is found in layer 2, but not layer 1
+            tier_1_cache->get(tier_1_cache, req);
+            tier_2_cache->get(tier_2_cache, req);
+            if(req->op == OP_READ){
+                multi_tier_stats.tier_1_read_miss++;
+                multi_tier_stats.tier_2_read_hit++;
+                multi_tier_stats.read_count++;
+            }else{
+                multi_tier_stats.tier_1_write_miss++;
+                multi_tier_stats.tier_2_write_hit++;
+                multi_tier_stats.write_count++;
+            }
+
+            multi_tier_stats.total_count++;
+            multi_tier_stats.tier_1_total_miss++;
+            multi_tier_stats.tier_2_total_hit++;
+        }
+
+        if((tier_1_check != cache_ck_miss) && (tier_2_check != cache_ck_miss)){
+            // Condition where the object is found in both layers. 
+            tier_1_cache->get(tier_1_cache, req);
+            tier_2_cache->get(tier_2_cache, req);
+
+            if(req->op == OP_READ){
+                multi_tier_stats.tier_1_read_hit++;
+                multi_tier_stats.tier_2_read_hit++;
+                multi_tier_stats.read_count++;
+            }else{
+                multi_tier_stats.tier_1_write_hit++;
+                multi_tier_stats.tier_2_write_hit++;
+                multi_tier_stats.write_count++;
+            }
+
+            multi_tier_stats.total_count++;
+            multi_tier_stats.tier_1_total_hit++;
+            multi_tier_stats.tier_2_total_hit++;
+        }
+    }
+
+    tier_1_cache->cache_free(tier_1_cache);
+    tier_2_cache->cache_free(tier_2_cache);
+
+    free_request(req);
+    close_reader(reader_csv);
+
+    FILE *out_file = fopen(op_fname, "a");
+    fprintf(out_file, "%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", multi_tier_stats.total_count, multi_tier_stats.read_count,
+     multi_tier_stats.write_count, multi_tier_stats.tier_1_read_hit, multi_tier_stats.tier_2_read_hit, multi_tier_stats.tier_1_read_miss, 
+     multi_tier_stats.tier_2_read_miss, multi_tier_stats.tier_1_write_hit, multi_tier_stats.tier_2_write_hit, multi_tier_stats.tier_1_write_miss,
+     multi_tier_stats.tier_2_write_miss, multi_tier_stats.tier_1_total_hit, multi_tier_stats.tier_2_total_hit, multi_tier_stats.tier_1_total_miss, 
+     multi_tier_stats.tier_2_total_miss);
+
+     fclose(out_file);
+
+}
+
+int main(int argc, char* argv[]){
+
+    uint64_t tier_1_size = atoi(argv[2]);
+    uint64_t tier_2_size = atoi(argv[3]);
+
+    compute_hits(argv[1], tier_1_size, tier_2_size, argv[4]);
+
+}
