d42dda47b97602cee949ea2db4dee9669d7f028d
thebrahminator <vishwa.nath@outlook.com>
2020-09-15 18:05:29 -0400

    Added stat collection for exclusive

--- multitiersimulator.c
+++ multitiersimulator.c
@@ -24,8 +24,21 @@ typedef struct{
     int write_count;
     int total_count;
     
+    int tier_2_not_checked; // Only for exclusive caches, if there's a hit in layer 1. 
 }traceStats;
 
+
+void write_stat_to_file(char* op_fname, traceStats multi_tier_stats){
+    FILE *out_file = fopen(op_fname, "a");
+
+    fprintf(out_file, "%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", multi_tier_stats.total_count, multi_tier_stats.read_count,
+     multi_tier_stats.write_count, multi_tier_stats.tier_1_read_hit, multi_tier_stats.tier_2_read_hit, multi_tier_stats.tier_1_read_miss, 
+     multi_tier_stats.tier_2_read_miss, multi_tier_stats.tier_1_write_hit, multi_tier_stats.tier_2_write_hit, multi_tier_stats.tier_1_write_miss,
+     multi_tier_stats.tier_2_write_miss, multi_tier_stats.tier_1_total_hit, multi_tier_stats.tier_2_total_hit, multi_tier_stats.tier_1_total_miss, 
+     multi_tier_stats.tier_2_total_miss, multi_tier_stats.tier_2_not_checked);
+
+    fclose(out_file);
+}
 
 void inclusive_caching(char* filename, uint64_t tier_1_size, uint64_t tier_2_size, char* op_fname){
     
@@ -45,7 +58,7 @@ void inclusive_caching(char* filename, uint64_t tier_1_size, uint64_t tier_2_siz
     cache_t *tier_1_cache = create_cache("LRU", tier_1_params, NULL);
     cache_t *tier_2_cache = create_cache("LRU", tier_2_params, NULL);
 
-    traceStats multi_tier_stats = {0,0,0,0,0,0,0,0,0};
+    traceStats multi_tier_stats = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1};
 
     while(read_one_req(reader_csv, req) == 0){
         cache_ck_res_e tier_1_check = tier_1_cache->check(tier_1_cache, req, true);
@@ -115,14 +128,15 @@ void inclusive_caching(char* filename, uint64_t tier_1_size, uint64_t tier_2_siz
     free_request(req);
     close_reader(reader_csv);
 
-    FILE *out_file = fopen(op_fname, "a");
-    fprintf(out_file, "%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", multi_tier_stats.total_count, multi_tier_stats.read_count,
-     multi_tier_stats.write_count, multi_tier_stats.tier_1_read_hit, multi_tier_stats.tier_2_read_hit, multi_tier_stats.tier_1_read_miss, 
-     multi_tier_stats.tier_2_read_miss, multi_tier_stats.tier_1_write_hit, multi_tier_stats.tier_2_write_hit, multi_tier_stats.tier_1_write_miss,
-     multi_tier_stats.tier_2_write_miss, multi_tier_stats.tier_1_total_hit, multi_tier_stats.tier_2_total_hit, multi_tier_stats.tier_1_total_miss, 
-     multi_tier_stats.tier_2_total_miss);
+    // FILE *out_file = fopen(op_fname, "a");
+    // fprintf(out_file, "%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", multi_tier_stats.total_count, multi_tier_stats.read_count,
+    //  multi_tier_stats.write_count, multi_tier_stats.tier_1_read_hit, multi_tier_stats.tier_2_read_hit, multi_tier_stats.tier_1_read_miss, 
+    //  multi_tier_stats.tier_2_read_miss, multi_tier_stats.tier_1_write_hit, multi_tier_stats.tier_2_write_hit, multi_tier_stats.tier_1_write_miss,
+    //  multi_tier_stats.tier_2_write_miss, multi_tier_stats.tier_1_total_hit, multi_tier_stats.tier_2_total_hit, multi_tier_stats.tier_1_total_miss, 
+    //  multi_tier_stats.tier_2_total_miss);
 
-     fclose(out_file);
+    //  fclose(out_file);
+    write_stat_to_file(op_fname, multi_tier_stats);
 
 }
 
@@ -144,25 +158,66 @@ void exclusive_caching(char *filename, uint64_t tier_1_size, uint64_t tier_2_siz
     cache_t *tier_1_cache = create_cache("LRU", tier_1_params, NULL);
     cache_t *tier_2_cache = create_cache("LRU", tier_2_params, NULL);
 
+    traceStats exclusive_stats = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1};
+    exclusive_stats.tier_2_not_checked++;
     while(read_one_req(reader_csv, req) == 0){
         cache_ck_res_e tier_1_check = tier_1_cache->check(tier_1_cache, req, true);
-        cache_ck_res_e tier_2_check = tier_2_cache->check(tier_2_cache, req, true); 
+        cache_ck_res_e tier_2_check = tier_2_cache->check(tier_2_cache, req, true);
+        exclusive_stats.total_count++;
         print_request(req);
 
         if(unlikely(tier_1_cache->occupied_size < tier_1_cache->cache_size)){
+            exclusive_stats.tier_2_not_checked++;
             if(tier_1_check == cache_ck_hit){
                 printf("Tier 1 cache hit. Size lower.\n");
                 tier_1_cache->get(tier_1_cache, req);
+                if(req->op == OP_WRITE){
+                    exclusive_stats.write_count++;
+                    exclusive_stats.tier_1_write_hit++;
+                }else{
+                    exclusive_stats.read_count++;
+                    exclusive_stats.tier_1_read_hit++;
+                }
+                exclusive_stats.tier_1_total_hit;
             }else{
                 printf("Tier 1 cache miss. Size lower.\n");
                 tier_1_cache->get(tier_1_cache, req);
+                if(req->op == OP_WRITE){
+                    exclusive_stats.write_count++;
+                    exclusive_stats.tier_1_write_miss++;
+                }else{
+                    exclusive_stats.read_count++;
+                    exclusive_stats.tier_1_read_miss++;
+                }
             } 
         }else{
             if(tier_1_check == cache_ck_hit){
+                exclusive_stats.tier_2_not_checked++;
                 printf("Tier 1 cache hit. Size higher.\n");
                 tier_1_cache->get(tier_1_cache, req);
+                if(req->op == OP_WRITE){
+                    exclusive_stats.write_count++;
+                    exclusive_stats.tier_1_write_hit++;
+                }else{
+                    exclusive_stats.read_count++;
+                    exclusive_stats.tier_1_read_hit++;
+                }
+                exclusive_stats.tier_1_total_hit++;
             }else if((tier_1_check == cache_ck_miss) && (tier_2_check == cache_ck_hit)){
                 printf("Tier 1 miss. Tier 2 hit. Size lower.\n");
+                if(req->op == OP_READ){
+                    exclusive_stats.read_count++;
+                    exclusive_stats.tier_1_read_miss++;
+                    exclusive_stats.tier_2_read_hit++;
+                    exclusive_stats.tier_1_total_miss++;
+                    exclusive_stats.tier_2_total_hit++;
+                }else{
+                    exclusive_stats.write_count++;
+                    exclusive_stats.tier_1_write_miss++;
+                    exclusive_stats.tier_2_write_hit++;
+                    exclusive_stats.tier_1_total_miss++;
+                    exclusive_stats.tier_2_total_hit++;                
+                }
                 cache_obj_t evicted_object;
                 tier_1_cache->evict(tier_1_cache, req, &evicted_object);
                 tier_1_cache->get(tier_1_cache, req);
@@ -170,6 +225,20 @@ void exclusive_caching(char *filename, uint64_t tier_1_size, uint64_t tier_2_siz
                 req->op = OP_WRITE;
                 tier_2_cache->get(tier_2_cache, req);
             }else if((tier_1_check == cache_ck_miss) && (tier_1_check == cache_ck_miss)){
+                printf("Tier 1 miss. Tier 2 hit. Size lower.\n");
+                if(req->op == OP_READ){
+                    exclusive_stats.read_count++;
+                    exclusive_stats.tier_1_read_miss++;
+                    exclusive_stats.tier_2_read_miss++;
+                    exclusive_stats.tier_1_total_miss++;
+                    exclusive_stats.tier_2_total_miss++;
+                }else{
+                    exclusive_stats.write_count++;
+                    exclusive_stats.tier_1_write_miss++;
+                    exclusive_stats.tier_2_write_miss++;
+                    exclusive_stats.tier_1_total_miss++;
+                    exclusive_stats.tier_2_total_miss++;                
+                }
                 printf("Miss in both tiers\n");
                 cache_obj_t evicted_object;
                 tier_1_cache->evict(tier_1_cache, req, &evicted_object);
@@ -187,6 +256,8 @@ void exclusive_caching(char *filename, uint64_t tier_1_size, uint64_t tier_2_siz
 
     free_request(req);
     close_reader(reader_csv);
+
+    write_stat_to_file(op_fname, exclusive_stats);
 }
 int main(int argc, char* argv[]){
 
